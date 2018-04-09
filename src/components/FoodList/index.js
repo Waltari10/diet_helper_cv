2365ad336e2e6b54b1d70aa739400fd71a8c1c44
Valtteri Laine <valtteri@meruhealth.com>
2018-04-09 10:28:31 +0300

    moved csv

--- src/components/FoodList/index.js
+++ src/components/FoodList/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import style from './styles.css'
+import food_data from './food_data.csv'
+console.log(food_data)
 
 class FoodList extends Component {
+
   render() {
     return (
       <div>
+        {food_data}
       </div>
     );
   }
