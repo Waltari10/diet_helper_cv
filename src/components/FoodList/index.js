eadb795a28e0433f782c48b752601e6a65a22570
Valtteri Laine <valtteri@meruhealth.com>
2018-04-18 08:10:57 +0300

    Renders list of food items and parses csv file

--- src/components/FoodList/index.js
+++ src/components/FoodList/index.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react';
 import style from './styles.css'
-import food_data from './food_data.csv'
-console.log(food_data)
+import Papa from 'papaparse'
 
 class FoodList extends Component {
+  state = {
+    foodData: []
+  }
+  constructor(props) {
+    super(props)
+    fetch('./food_data.csv')
+    .then(response => response.text())
+    .then(csv => {
+      var foodData = this.parseCSV(csv)
+      this.setState({
+        foodData,
+      })
 
+    })
+  }
+  parseCSV(csv) {
+    const csvRows = csv.split('\n')
+    const keys = csvRows[0].split(",")
+
+    const objArray = csvRows.map(rowText => {
+      const row = rowText.split(',')
+      const obj = {}
+
+      for (let i = 0; i < keys.length; i++) {
+        obj[keys[i]] = row[i]
+      }
+
+      return obj
+    })
+
+    return objArray
+  }
+  renderFoodItem(food) {
+    return (
+      <div
+        key={food.food}
+      >
+        <p>
+          {food.food}
+        </p>
+      </div>
+    )
+  }
+  renderFoodList() {
+    return this.state.foodData.map(this.renderFoodItem)
+  }
   render() {
     return (
       <div>
-        {food_data}
+        {this.renderFoodList()}
       </div>
     );
   }
