18da239b9d0e28bf06e598e7b76efc170f058ae6
Valtteri Laine <valtteri.e.laine@gmail.com>
2018-08-10 11:33:58 +0300

    Eslint fix and npm audit fix

--- src/components/LanguagePicker/index.js
+++ src/components/LanguagePicker/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent  } from 'react'
+import React, { PureComponent } from 'react'
 import AngleDown from 'react-icons/lib/fa/angle-down'
 import AngleUp from 'react-icons/lib/fa/angle-up'
 import './styles.css'
